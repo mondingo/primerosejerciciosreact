7a2ff82d2d0235106eef074ec71c53fecc3ce614
m0nd1 <rincondemondi@gmail.com>
2026-01-19 05:51:06 +0100

    Cambiando dist por docs para GitHub 4

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // Esta línea le dice a Vite que tu web vive en esa subcarpeta de GitHub
   base: '/primerosejerciciosreact/', 
   plugins: [react()],
   build: {
-    outDir: 'docs', // Para que el build vaya a la carpeta que GitHub Pages está leyendo
+    outDir: 'docs', // Asegúrate de que coincida con la carpeta que lee GitHub Pages
   }
 })
