20d27e8cb2861499c14070b6d556ba2b13734064
m0nd1 <rincondemondi@gmail.com>
2026-01-19 05:32:04 +0100

    Cambiando dist por docs para GitHub

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: '/Ejercicios-variados-con-React/', 
+  build: {
+    outDir: 'docs', // Ahora Vite escribirá en /docs en lugar de /dist
+  },
+  plugins: [react()],
 })
